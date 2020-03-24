3fffd71e1caa3fe516d0e9812b57715844d18495
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-25 02:36:33 +0430

    Implementing Intent Buttons

--- app/src/main/java/com/taghavi/covid_19detector/fragments/HomeFragment.kt
+++ app/src/main/java/com/taghavi/covid_19detector/fragments/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.taghavi.covid_19detector.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,8 @@ import androidx.databinding.DataBindingUtil
 
 import com.taghavi.covid_19detector.R
 import com.taghavi.covid_19detector.databinding.FragmentHomeBinding
+import com.taghavi.covid_19detector.utilities.CAMERA_ID
+import com.taghavi.covid_19detector.utilities.GALLERY_ID
 
 class HomeFragment : Fragment() {
 
@@ -19,6 +23,20 @@ class HomeFragment : Fragment() {
     ): View? {
         val binding: FragmentHomeBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.homeOpenCameraButton.setOnClickListener {
+            //check permission here
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAMERA_ID)
+        }
+
+        binding.homeOpenGalleryButton.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            startActivityForResult(intent, GALLERY_ID)
+        }
 
         return binding.root
     }
---
+++ app/src/main/java/com/taghavi/covid_19detector/utilities/Constants.kt
@@ -0,0 +1,4 @@
+package com.taghavi.covid_19detector.utilities
+
+const val CAMERA_ID = 8267
+const val GALLERY_ID = 4912
