a780d10ee41504b2bb6af18e917da79f4eaab95e
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-30 23:49:19 +0430

    Implement UploadAPIs interface

--- app/src/main/java/com/taghavi/covid_19detector/fragments/HomeFragment.kt
+++ app/src/main/java/com/taghavi/covid_19detector/fragments/HomeFragment.kt
@@ -24,6 +24,13 @@ import com.taghavi.covid_19detector.R
 import com.taghavi.covid_19detector.apiServices.VolleyApiService
 import com.taghavi.covid_19detector.databinding.FragmentHomeBinding
 import com.taghavi.covid_19detector.utilities.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -194,5 +201,14 @@ class HomeFragment : Fragment() {
                 MyLog.i(e.toString())
             }
         }
+    }
+
+    interface UploadAPIs {
+        @Multipart
+        @POST("/upload")
+        fun uploadImage(
+            @Part file: MultipartBody.Part,
+            @Part("name") requestBody: RequestBody
+        ): Call<ResponseBody>
     }
 }
