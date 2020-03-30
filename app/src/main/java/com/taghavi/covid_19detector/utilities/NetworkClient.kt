72b55ce66baf954bdbc087ab2a82adfac2829e75
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-30 23:59:51 +0430

    convert NetworkClient to kotlin

---
+++ app/src/main/java/com/taghavi/covid_19detector/utilities/NetworkClient.kt
@@ -0,0 +1,23 @@
+package com.taghavi.covid_19detector.utilities
+
+import android.content.Context
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkClient {
+    private const val BASE_URL = ""
+    private var retrofit: Retrofit? = null
+    fun getRetrofitClient(context: Context?): Retrofit? {
+        if (retrofit == null) {
+            val okHttpClient = OkHttpClient.Builder()
+                .build()
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return retrofit
+    }
+}
